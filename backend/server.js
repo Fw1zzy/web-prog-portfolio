6095205289a60bf6fbff60be619931dd6c76cbc6
Fw1zzy <emmanuelpascua.ph@gmail.com>
2026-04-17 08:41:32 +0800

    frontend4
    frontend4

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, "http://localhost:5173"] : "https://portfolio-backend-i1da.onrender.com/api",
+  origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, "http://localhost:5173"] : "http://localhost:5173",
   credentials: true,
   optionsSuccessStatus: 200,
 };
